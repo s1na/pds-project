86bd6756bf4020a56ef5a1702c3c0c7e20320f05
Omar Trinidad Gutiérrez Méndez <omar.vpa@gmail.com>
2016-01-20 21:54:55 +0100

    Ignore .class files

--- java/Main.java
+++ java/Main.java
@@ -1,66 +1,87 @@
 
-import java.net.BindException;
-import java.net.ServerSocket;
-import java.net.InetAddress;
-import java.io.IOException;
-import java.util.Scanner;
-
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
-
 import org.apache.commons.cli.ParseException;
-import org.apache.commons.cli.UnrecognizedOptionException;
+
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.Scanner;
+
+class Node {
+    String ip = "100";
+}
 
 public class Main {
 
+    /* JSON request over HTTP */
+    //public static void requestJSONHttp(String address) throws Exception {
+    public static void requestJSONHttp(String address) {
+
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpPost httppost = new HttpPost(address);
+        httppost.setHeader("Content-Type", "application/json");
+
+        Gson gson = new GsonBuilder().create();
+        Node myNode = new Node();
+
+        httppost.setEntity(new StringEntity(gson.toJson(myNode), "UTF-8"));
+
+    }
+
     public static void main(String[] args) {
 
-        int port;
+        int port = 8080;
         boolean alive = true;
 
-        if (args.length == 0) {
-            port = 8080;
-        } else { 
-            port = Integer.parseInt(args[0]);
-        }
-
-        //Options options = new Options();
-        //options.addOption("j", true, "Join");
-        //CommandLineParser parser = new DefaultParser();
-        //String[] argss = new String[]{ "--block-size=10" };
+        Options options = new Options();
+        options.addOption("p", "port", true, "Port");
+        CommandLineParser parser = new DefaultParser();
 
         try {
-            while(alive) {
-                System.out.print(">>> ");
-                Scanner in = new Scanner(System.in);
-                String s = in.nextLine();
-                // ToDo: create exceptions
-                
-                if ( s.equals("join") ) {
-                    //ToDo
-                    //join(address, port);
-                    ServerSide socketServer = new ServerSide(port);
-                    socketServer.start();
-                } else if ( s.equals("exit") || s.equals("signoff") ) {
-                    //ToDo
-                    //leave the group, update the network
-                    alive = false;
-                    //leave();
-                } else if ( s.equals("list") ) {
-                    System.out.println("ToDo: List");
-                } else if ( s.equals("start") ) {
-                    System.out.println("ToDo: Start");
-                } else if ( s.equals("42") ) {
-                    System.out.println("Die Antwort nach dem Leben, dem " +  
-                                       "Universum und allem");
-                } else if ( !s.equals("") ) {
-                    System.out.println("Unknown command");
-                }
+
+            CommandLine line = parser.parse(options, args);
+            if ( line.hasOption( "port" ) ) {
+                port = Integer.parseInt( line.getOptionValue("port") );
             }
-        } catch (IOException e) {
-            System.err.print(e.getMessage());
+
+        } catch (ParseException exp) {
+            System.err.println("Parsing failed. Reason " + exp.getMessage() );
+        }
+
+
+        while(alive) {
+            System.out.print(">>> ");
+            Scanner in = new Scanner(System.in);
+            String s = in.nextLine();
+            // ToDo: create exceptions
+            
+            if ( s.equals("join") ) {
+                //ToDo
+                requestJSONHttp("http://192.168.71.43:8081/join");
+                
+            } else if ( s.equals("exit") || s.equals("signoff") ) {
+                //ToDo
+                //leave the group, update the network
+                alive = false;
+                //leave();
+            } else if ( s.equals("list") ) {
+                System.out.println("ToDo: List");
+            } else if ( s.equals("start") ) {
+                System.out.println("ToDo: Start");
+            } else if ( s.equals("42") ) {
+                System.out.println("Die Antwort nach dem Leben, dem " +  
+                                   "Universum und allem");
+            } else if ( !s.equals("") ) {
+                System.out.println("Unknown command");
+            }
         }
     }
 }
